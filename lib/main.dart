0f026ea65d6dcede886dc14cdf245e1ee654dff3
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-10 21:31:38 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/core/presentation/components/big_button.dart';
+import 'package:flutter_recipe_app/core/presentation/components/medium_button.dart';
+import 'package:flutter_recipe_app/ui/text_styles.dart';
+
+import 'core/presentation/components/small_button.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Components', style: TextStyles.largeTextBold),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: BigButton(
+              'Big Button',
+              onPressed: () {
+                print('big button');
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MediumButton(
+              'medium button',
+              onPressed: () {
+                print('medium button');
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SmallButton(
+              'small button',
+              onPressed: () {
+                print('small button');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
