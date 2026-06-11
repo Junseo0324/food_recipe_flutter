95626155173080836161635da2d7deb16b514300
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-11 19:14:34 +0900

    router

---
+++ lib/core/routing/router.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/presentation/sign_in/sign_in_screen.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../data/repository/mock_bookmark_repository_impl.dart';
+import '../../data/repository/mock_recipe_repository_impl.dart';
+import '../../domain/model/recipe.dart';
+import '../../domain/use_case/get_saved_recipes_use_case.dart';
+import '../../presentation/saved_recipes/saved_recipes_screen.dart';
+import '../../presentation/splash/splash_screen.dart';
+
+final router = GoRouter(
+  initialLocation: '/Splash',
+  routes: [
+    GoRoute(
+      path: '/Splash',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(path: '/SingIn', builder: (context, state) => const SignInScreen()),
+    GoRoute(
+        path: '/SavedRecipes',
+      builder: (context, state) => FutureBuilder<List<Recipe>>(
+        future:
+        GetSavedRecipesUseCase(
+          recipeRepository: MockRecipeRepositoryImpl(),
+          bookmarkRepository: MockBookmarkRepositoryImpl(),
+        ).execute(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final recipes = snapshot.data!;
+
+          return SavedRecipesScreen(recipes: recipes);
+        },
+      ),
+    ),
+  ],
+);
