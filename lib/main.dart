9026cd6cf3f0f259a7a8b1509d16497989d15afa
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-11 18:45:17 +0900

    saved_recipes_screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,11 @@ import 'package:flutter_recipe_app/core/presentation/components/medium_button.da
 import 'package:flutter_recipe_app/core/presentation/components/rating_button.dart';
 import 'package:flutter_recipe_app/core/presentation/components/two_tabs.dart';
 import 'package:flutter_recipe_app/core/presentation/dialogs/rating_dialog.dart';
+import 'package:flutter_recipe_app/data/repository/mock_bookmark_repository_impl.dart';
+import 'package:flutter_recipe_app/data/repository/mock_recipe_repository_impl.dart';
+import 'package:flutter_recipe_app/domain/model/recipe.dart';
+import 'package:flutter_recipe_app/domain/use_case/get_saved_recipes_use_case.dart';
+import 'package:flutter_recipe_app/presentation/saved_recipes/saved_recipes_screen.dart';
 import 'package:flutter_recipe_app/presentation/sign_in/sign_in_screen.dart';
 import 'package:flutter_recipe_app/ui/text_styles.dart';
 
@@ -22,10 +27,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData(colorScheme: ColorScheme.light()),
+      home: FutureBuilder<List<Recipe>>(
+        future:
+            GetSavedRecipesUseCase(
+              recipeRepository: MockRecipeRepositoryImpl(),
+              bookmarkRepository: MockBookmarkRepositoryImpl(),
+            ).execute(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final recipes = snapshot.data!;
+
+          return SavedRecipesScreen(recipes: recipes);
+        },
       ),
-      home: const MyHomePage(),
     );
   }
 }
