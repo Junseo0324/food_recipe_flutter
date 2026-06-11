8e4e52c4fb18d5cee0144345b20f1847fb872881
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-11 18:11:09 +0900

    model

---
+++ lib/domain/model/recipe.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_recipe_app/domain/model/recipe_ingredient.dart';
+
+class Recipe {
+  final String category;
+  final int id;
+  final String name;
+  final String image;
+  final String chef;
+  final String time;
+  final double rating;
+  final List<RecipeIngredient> ingredients;
+
+  Recipe({
+    required this.id,
+    required this.category,
+    required this.name,
+    required this.image,
+    required this.chef,
+    required this.time,
+    required this.rating,
+    required this.ingredients,
+  });
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      id: json['id'] as int,
+      category: json['category'] as String,
+      name: json['name'] as String,
+      image: json['image'] as String,
+      chef: json['chef'] as String,
+      time: json['time'] as String,
+      rating: json['rating'] as double,
+      ingredients: (json['ingredients'] as List<dynamic>?)
+          ?.map((e) => RecipeIngredient.fromJson(e as Map<String, dynamic>))
+          .toList() ?? [],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'category': category,
+      'name': name,
+      'image': image,
+      'chef': chef,
+      'time': time,
+      'rating': rating,
+      'ingredients': ingredients.map((e) => e.toJson()).toList(),
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Recipe(id: $id, category: $category, name: $name, chef: $chef, time: $time, rating: $rating, ingredients: $ingredients)';
+  }
+}
