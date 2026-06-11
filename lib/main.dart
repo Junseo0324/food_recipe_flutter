9edb8c7a92b5b9308786ae01151a444072a71b90
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-11 19:14:29 +0900

    splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_recipe_app/core/presentation/components/big_button.dart';
-import 'package:flutter_recipe_app/core/presentation/components/filter_button.dart';
-import 'package:flutter_recipe_app/core/presentation/components/input_field.dart';
-import 'package:flutter_recipe_app/core/presentation/components/medium_button.dart';
-import 'package:flutter_recipe_app/core/presentation/components/rating_button.dart';
-import 'package:flutter_recipe_app/core/presentation/components/two_tabs.dart';
-import 'package:flutter_recipe_app/core/presentation/dialogs/rating_dialog.dart';
-import 'package:flutter_recipe_app/data/repository/mock_bookmark_repository_impl.dart';
-import 'package:flutter_recipe_app/data/repository/mock_recipe_repository_impl.dart';
-import 'package:flutter_recipe_app/domain/model/recipe.dart';
-import 'package:flutter_recipe_app/domain/use_case/get_saved_recipes_use_case.dart';
-import 'package:flutter_recipe_app/presentation/saved_recipes/saved_recipes_screen.dart';
+import 'package:flutter_recipe_app/core/routing/router.dart';
 import 'package:flutter_recipe_app/presentation/sign_in/sign_in_screen.dart';
-import 'package:flutter_recipe_app/ui/text_styles.dart';
 
-import 'core/presentation/components/small_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,25 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: ColorScheme.light()),
-      home: FutureBuilder<List<Recipe>>(
-        future:
-            GetSavedRecipesUseCase(
-              recipeRepository: MockRecipeRepositoryImpl(),
-              bookmarkRepository: MockBookmarkRepositoryImpl(),
-            ).execute(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          final recipes = snapshot.data!;
-
-          return SavedRecipesScreen(recipes: recipes);
-        },
-      ),
     );
   }
 }
