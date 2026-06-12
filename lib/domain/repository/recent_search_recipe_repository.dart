98b480992045fe4e59c7919032987755f98db4eb
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-13 00:46:00 +0900

    recent_search_repository

---
+++ lib/domain/repository/recent_search_recipe_repository.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_recipe_app/domain/model/recipe.dart';
+
+abstract interface class RecentSearchRecipeRepository {
+  Future<List<Recipe>> getRecentSearchRecipes();
+
+  Future<void> updateRecentSearchRecipes(List<Recipe> recipes);
+}
