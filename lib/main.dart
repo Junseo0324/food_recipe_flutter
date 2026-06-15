75e32abb60b29d27019af2739ee44d51f4552eae
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-15 23:31:38 +0900

    feat: di

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/core/di/di_setup.dart';
 import 'package:flutter_recipe_app/core/routing/router.dart';
 import 'package:flutter_recipe_app/presentation/sign_in/sign_in_screen.dart';
 
 
 void main() {
+  diSetup();
   runApp(const MyApp());
 }
 
--- lib/presentation/search/search_root.dart
+++ lib/presentation/search/search_root.dart
@@ -1,33 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_recipe_app/data/data_source/local/default_local_storage.dart';
-import 'package:flutter_recipe_app/data/data_source/local_storage.dart';
-import 'package:flutter_recipe_app/data/data_source/remote/remote_recipe_data_source_impl.dart';
-import 'package:flutter_recipe_app/data/repository/mock_recent_search_recipe_repository_impl.dart';
-import 'package:flutter_recipe_app/data/repository/mock_recipe_repository_impl.dart';
-import 'package:flutter_recipe_app/domain/use_case/search_recipes_use_case.dart';
+import 'package:flutter_recipe_app/core/di/di_setup.dart';
 import 'package:flutter_recipe_app/presentation/search/search_screen.dart';
 import 'package:flutter_recipe_app/presentation/search/search_view_model.dart';
-
-final LocalStorage _localStorage = DefaultLocalStorage();
-
-final _recentSearchRecipeRepository = MockRecentSearchRecipeRepositoryImpl(
-  localStorage: _localStorage,
-);
 
 class SearchRoot extends StatelessWidget {
   const SearchRoot({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = SearchViewModel(
-      recentSearchRecipeRepository: _recentSearchRecipeRepository,
-      searchRecipesUseCase: SearchRecipesUseCase(
-        recipeRepository: MockRecipeRepositoryImpl(
-          recipeDataSource: RemoteRecipeDataSourceImpl(),
-        ),
-        localStorage: _localStorage,
-      ),
-    );
+    final viewModel = getIt<SearchViewModel>();
     return ListenableBuilder(
       listenable: viewModel,
       builder: (context, widget) {
