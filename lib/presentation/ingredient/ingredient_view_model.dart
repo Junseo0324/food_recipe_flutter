8a76e4978b34db08863f4deeef8e9473bc97f126
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-27 17:48:40 +0900

    clipboard add

--- lib/presentation/ingredient/ingredient_view_model.dart
+++ lib/presentation/ingredient/ingredient_view_model.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/domain/clipboard/clipboard_service.dart';
 import 'package:flutter_recipe_app/domain/repository/procedure_repository.dart';
 import 'package:flutter_recipe_app/domain/use_case/get_dishes_by_category_use_case.dart';
 import 'package:flutter_recipe_app/presentation/ingredient/ingredient_action.dart';
@@ -12,6 +13,7 @@ class IngredientViewModel with ChangeNotifier {
   final IngredientRepository _ingredientRepository;
   final ProcedureRepository _procedureRepository;
   final GetDishesByCategoryUseCase _getDishesByCategoryUseCase;
+  final ClipboardService _clipboardService;
 
   IngredientState _state = IngredientState();
 
@@ -21,9 +23,11 @@ class IngredientViewModel with ChangeNotifier {
     required IngredientRepository ingredientRepository,
     required ProcedureRepository procedureRepository,
     required GetDishesByCategoryUseCase getDishesByCategoryUseCase,
+    required ClipboardService clipboardService,
   }) : _ingredientRepository = ingredientRepository,
        _procedureRepository = procedureRepository,
-       _getDishesByCategoryUseCase = getDishesByCategoryUseCase;
+       _getDishesByCategoryUseCase = getDishesByCategoryUseCase,
+       _clipboardService = clipboardService;
 
   void onAction(IngredientAction action) async {
     switch (action) {
@@ -38,11 +42,11 @@ class IngredientViewModel with ChangeNotifier {
         notifyListeners();
       case OnTapShareMenu():
         log(action.link);
+        _clipboardService.copyText(action.link);
       case OnTapRateRecipe():
         log('${action.rate}');
       case OnTapUnsave():
         // TODO: Handle this case.
-        throw UnimplementedError();
       case OnTapFollow():
     }
   }
